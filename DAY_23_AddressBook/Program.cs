a9d394caef84500ef723db8c8dd744f19a42640e
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-03 20:34:51 +0530

    [Shantanu] Added: Main method and Welcome note to the Address book program.

--- DAY_23_AddressBook/Program.cs
+++ DAY_23_AddressBook/Program.cs
@@ -6,34 +6,12 @@ namespace DAY_23_AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Address Book System");
+            Console.WriteLine("Welcome to Address book program");
 
-
-            while (true)
-            {
-                Console.WriteLine("\n1. Add member to Contact list \n2.View Members in Contact List \n3.Edit Contact " +
-                    "\n4.Exit");
-                Console.Write("Enter an option:");
-                switch (Convert.ToInt32(Console.ReadLine()))
-                {
-                    case 1:
-                        AddressBook.AddMember();
-                        break;
-
-                    case 2:
-                        AddressBook.ViewContacts();
-                        break;
-
-                    case 3:
-                        AddressBook.EditDetails();
-                        break;
-
-
-                    case 4:
-                        Console.WriteLine("Exited");
-                        return;
-                }
-            }
+            AddressBook.AddContact();
+            AddressBook.ContactsDisplay();
+            AddressBook.EditContact();
+            AddressBook.ContactsDisplay();
         }
     }
 }
