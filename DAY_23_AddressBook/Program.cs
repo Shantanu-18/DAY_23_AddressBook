f7b1bbce8a962f6de6ffe9de4f2be79f000e888f
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-03 20:50:39 +0530

    [Shantanu] Added: Main method and Welcome note to the Address book program.

--- DAY_23_AddressBook/Program.cs
+++ DAY_23_AddressBook/Program.cs
@@ -6,41 +6,20 @@ namespace DAY_23_AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Address Book System");
-            AddressBook obj = new AddressBook();
+            Console.WriteLine("Welcome to Address book program");
 
-
-            while (true)
-            {
-                Console.WriteLine("1. Add member to Contact list \n2. View Members in Contact List" +
-                                                  "\n3. Edit Contact\n4. Delete Contact\n5. Exit");
-                Console.Write("Enter an option:");
-
-                switch (Convert.ToInt32(Console.ReadLine()))
-                {
-                    case 1:
-                        obj.AddMember();
-                        break;
-                        
-                    case 2:
-                        obj.ViewContacts();
-                        break;
-                       
-                    case 3:
-                        obj.EditDetails();
-                        break;
-                        
-                    case 4:
-                        obj.DeleteDetails();
-                        break;
-                       
-
-                    case 5:
-                        Console.WriteLine("Exited");
-                        return;
-
-                }
-            }
+            AddressBook.addAddressBook();
+            Console.WriteLine("\n");
+            AddressBook.ContactsDisplay();
+            Console.WriteLine("\n");
+            AddressBook.EditContact();
+            Console.WriteLine("\n");
+            AddressBook.ContactsDisplay();
+            Console.WriteLine("\n");
+            AddressBook.ContactsDisplay();
+            AddressBook.ContactsDisplay();
+            AddressBook.ContactsDisplay();
+            AddressBook.ContactsDisplay();
         }
     }
 }
