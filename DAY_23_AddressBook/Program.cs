131537893dec1fce44f6abfc2273bb1d628e87fe
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-03 13:00:26 +0530

    [Shantanu] Added : Main method and asking user for options.

--- DAY_23_AddressBook/Program.cs
+++ DAY_23_AddressBook/Program.cs
@@ -11,15 +11,20 @@ namespace DAY_23_AddressBook
 
             while (true)
             {
-                Console.WriteLine("\n1. Add member to Contact list \n2.Exit");
+                Console.WriteLine("\n1. Add member to Contact list \n2.View Members in Contact List \n3.Exit");
                 Console.Write("Enter an option:");
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
                     case 1:
                         AddressBook.AddMember();
                         break;
-                    
+
                     case 2:
+                        AddressBook.ViewContacts();
+                        break;
+
+
+                    case 3:
                         Console.WriteLine("Exited");
                         return;
                 }
