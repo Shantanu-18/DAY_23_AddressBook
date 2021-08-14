6ac8caf0913363375ebeba474c9d573a3608512e
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-14 20:06:26 +0530

    [Shantanu] Added : Class which contains properties of details of person in address book.

--- DAY_23_AddressBook/Contacts.cs
+++ DAY_23_AddressBook/Contacts.cs
@@ -4,15 +4,24 @@ using System.Text;
 
 namespace DAY_23_AddressBook
 {
+    [Serializable]
     class Contacts
     {
         public string firstName { get; set; }
+
         public string lastName { get; set; }
+
         public string address { get; set; }
+
         public string city { get; set; }
+
         public string state { get; set; }
+
         public string ZipCode { get; set; }
+
         public string PhoneNumber { get; set; }
+
         public string eMail { get; set; }
+
     }
 }
