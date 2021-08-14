c707a5461b713f3603929eff47686651ea9b1553
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-14 20:04:56 +0530

    [Shantanu] Added : Class to deserialise the serialised file.

---
+++ DAY_23_AddressBook/BinaryDeserialization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace DAY_23_AddressBook
+{
+    class BinaryDeserialization
+    {
+        public static Dictionary<string, List<Contacts>> Deserialization()
+        {
+            Dictionary<string, List<Contacts>> MySystem = new Dictionary<string, List<Contacts>>();
+            FileStream fileStream = new FileStream(@"E:\FellowShip\DAY_23_AddressBook\DAY_23_AddressBook\AddressBook.txt", FileMode.Open);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            MySystem = (Dictionary<string, List<Contacts>>)binaryFormatter.Deserialize(fileStream);
+            fileStream.Close();
+
+            return MySystem;
+
+        }
+    }
+}
