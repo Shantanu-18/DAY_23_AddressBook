3a39bc34717352513aa29fc15dafc5f80de75b2a
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-14 20:05:47 +0530

    [Shantanu] Added : Class to serialise the address book dict and creating txt file for it.

---
+++ DAY_23_AddressBook/BinarySerializations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace DAY_23_AddressBook
+{
+    class BinarySerializations
+    {
+        public static void Serializations(Dictionary<string, List<Contacts>> mySystem)
+        {
+            FileStream fileStream = new FileStream(@"E:\FellowShip\DAY_23_AddressBook\DAY_23_AddressBook\AddressBook.txt", FileMode.Create);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(fileStream, mySystem);
+            fileStream.Close();
+        }
+    }
+}
